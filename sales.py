d424335e65009232dc8b230a56639cc74acb9d4e
ograndebaam <s5525799@bournemouth.ac.uk>
2024-11-22 08:29:55 +0000

    Update sales.py

--- sales.py
+++ sales.py
@@ -19,5 +19,18 @@ available_items:(integer) This function returns this integer which updates the a
 The function will also update the inventory_records (For restocking) for a  given current day. 
 
     '''
+    # sets restocked_items back to 0 since there is no restocking on non-restocking days
+    restocked_inventory = 0
+
+    # Random generates the number of sold tshirts and keeps it less than the total of available items and less than 200
+    # Randomly generate the number of T-shirts sold, ensuring it's <= available items and <= 200
+    sales = random.randint(0, min(200, available_items))
     
+    # Update available_items by the sales
+    available_items -= sales
+
+    # Update the inventory log for the day
+    inventory_records.append((current_day, sales, restocked_inventory, available_items))
+
     return available_items
+
